074f6a497ce4271a2f6d685877d8ee452d56a395
tocteman <juanfrancisco.vinueza@gmail.com>
2018-10-30 09:47:12 -0500

    primer cambio

--- components/table/ng-table.component.ts
+++ components/table/ng-table.component.ts
@@ -26,7 +26,8 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
                  (tableChanged)="onChangeTable(config)"/>
         </td>
       </tr>
-        <tr *ngFor="let row of rows">
+        <tr *ngFor="let row of rows; let j = index">
+          <td>{{j + 1}}</td>
           <td (click)="cellClick(row, column.name)" *ngFor="let column of columns" [innerHtml]="sanitize(getData(row, column.name))"></td>
         </tr>
       </tbody>
